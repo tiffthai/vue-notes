9d9d04b019e55149c0770ff0e59cf645e1889813
tiffany-thai <67180329+tiffany-thai@users.noreply.github.com>
2021-07-29 20:56:37 -0700

    creates layout of notelist comp + minor styling

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,37 @@
 <template>
-	<NoteList />
+  <NoteList />
 </template>
 
 <script>
-import NoteList from './components/NoteList.vue'
+import NoteList from "./components/NoteList.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    NoteList
-  }
-}
+    NoteList,
+  },
+};
 </script>
 
 <style>
+html, body {
+	width: 100%;
+	height: 100%;
+	margin: 0;
+}
+
+input, textarea {
+	background: none;
+	border: none;
+	outline: none;
+	font-size: 16px;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	width: 100%;
+	height: 100%;
 }
 </style>
--- src/components/NoteList.vue
+++ src/components/NoteList.vue
@@ -1,16 +1,45 @@
 <template>
-	<div>
-		<div>
-			<input v-model="userInput" @keyup.enter="addNote" />
-			<button @click="addNote">Add note</button>
+	<div id="noteList">
+		<div id="noteHeadersContainer">
+			<div id="addNewNoteWrapper">
+				<input 
+					v-model="userInput" 
+					placeholder="add new note"
+					@keyup.enter="addNote" 
+					id="addNoteInput"
+				/>
+				<button @click="addNote">Add note</button>
+			</div>
+
+			<div
+				v-for="(listItem, listItemIndex) in listItems"
+				:key="'item' + listItemIndex"
+				:class="{activeListItem: listItemIndex === activeIndex}"
+				class="listItem"
+			>
+				<input 
+					v-model="listItem.header" 
+					@focus="handleOpenNoteContents(listItemIndex)" 
+					class="noteHeaderInput"
+				/>
+				<button @click="removeNote(listItemIndex)">delete</button>
+			</div>
 		</div>
 
-		<div
-			v-for="(listItem, listItemIndex) in listItems"
-			:key="'item' + listItemIndex"
-		>
-			<input v-model="listItem.text" />
-			<button @click="removeNote(listItemIndex)">delete</button>
+		<div id="noteDescriptionContainer">
+			<h2>{{ noteHeader }}</h2>
+			<div>
+				<textarea 
+					v-if="listItems.length"
+					:value="noteDescription" 
+					id="noteDescriptionTextarea"
+				/>
+			</div>
+
+			<div id="descriptionActionContainer">
+				<button>revert</button>
+				<button>save edit</button>
+			</div>
 		</div>
 	</div>
 </template>
@@ -22,16 +51,26 @@ export default {
 		return {
 			userInput: '',
 			listItems: [
-				{text: 'a'},
-				{text: 'b'},
-				{text: 'c'},
-			]
+				{header: 'header a', text: 'description of note goes here'},
+				{header: 'header b', text: 'description of note goes here'},
+				{header: 'header c', text: 'description of note goes here'},
+			],
+			activeIndex: 0
+		}
+	},
+	computed: {
+		noteHeader() {
+			return this.listItems[this.activeIndex].header;
+		},
+		noteDescription() {
+			return this.listItems[this.activeIndex].text;
 		}
 	},
 	methods: {
 		addNote() {
 			let newNote = {
-				text: this.userInput
+				header: this.userInput,
+				text: ''
 			}
 			this.listItems.push(newNote);
 
@@ -39,11 +78,66 @@ export default {
 		},
 		removeNote(listItemIndex) {
 			this.listItems.splice(listItemIndex, 1);
+		},
+		handleOpenNoteContents(listItemIndex) {
+			this.activeIndex = listItemIndex;
 		}
 	}
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+#noteList {
+	width: 100%;
+	height: 100%;
+	display: flex;
+}
 
+// left column styles
+#noteHeadersContainer {
+	flex: 3;
+	border-right: 2px solid #E9E9E9;
+}
+
+#addNewNoteWrapper {
+	padding: 12px 12px 36px 12px;
+	display: flex;
+}
+
+#noteDescriptionContainer {
+	flex: 2;
+	padding: 12px;
+}
+
+#addNoteInput {
+	background: #E9E9E9;
+	border-radius: 4px;
+	padding: 8px;
+}
+
+.listItem {
+	border-bottom: 1px solid #E9E9E9;
+	padding: 4px 12px;
+	display: flex;
+}
+
+.activeListItem {
+	background:#CBE5D5;
+}
+
+.noteHeaderInput {
+	width: 100%;
+}
+
+// right column styles
+#noteDescriptionTextarea {
+	width: 100%;
+	height: 100%;
+	resize: none;
+}
+
+#descriptionActionContainer {
+	display: flex;
+	justify-content: space-between;
+}
 </style>
